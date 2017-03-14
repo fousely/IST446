b6b7e10fd12b676ddb3e2492d1461bf6eb69717f
Stephen Fouse <stephen.fouse@gmail.com>
2017-03-14 14:35:49 -0400

    Added soldier asset; Finished basic board; Added lava and water blocks

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour {
+
+    private GameObject[] basicblocks;
+    private GameObject[] lavablocks;
+    private GameObject[] waterblocks;
+
+    public Material lavaMaterial;
+    public Material waterMaterial;
+
+	// Use this for initialization
+	void Start () {
+        basicblocks = GameObject.FindGameObjectsWithTag("Ground");
+        lavablocks = GameObject.FindGameObjectsWithTag("Lava");
+        waterblocks = GameObject.FindGameObjectsWithTag("Water");
+
+        foreach (GameObject block in lavablocks) {
+            for (int i = 1; i < block.transform.childCount; i++) {
+                block.transform.GetChild(i).GetComponent<Renderer>().material = lavaMaterial;
+            }
+        }
+        foreach (GameObject block in waterblocks) {
+            for (int i = 1; i < block.transform.childCount; i++) {
+                block.transform.GetChild(i).GetComponent<Renderer>().material = waterMaterial;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float panSpeed = 20f;
+    public float scrollSpeed = 5f;
+    public bool freeCamera = true;
+
+    public float miny = 10f;
+    public float maxy = 50f;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.L))
+            freeCamera = !freeCamera;
+        if (!freeCamera)
+            return;
+
+        if (Input.GetKey("w")/* || Input.mousePosition.y >= Screen.height - 10f*/) {
+            if ((transform.position + (Vector3.forward * panSpeed * Time.deltaTime)).z < 20f)
+                transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s")/* || Input.mousePosition.y <= 10f*/) {
+            if ((transform.position + (Vector3.forward * panSpeed * Time.deltaTime)).z > -21f)
+                transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a")/* || Input.mousePosition.x <= 10f*/) {
+            if ((transform.position + (Vector3.left * panSpeed * Time.deltaTime)).x > -30f)
+                transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d")/* || Input.mousePosition.x >= Screen.width - 10f*/) {
+            if ((transform.position + (Vector3.left * panSpeed * Time.deltaTime)).x < 10f)
+                transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scrollInfo = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 pos = transform.position;
+        pos.y -= scrollInfo * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, miny, maxy);
+        transform.position = pos;
+    }
+}
