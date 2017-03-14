088b7f43201bce3d12649a23ca0caddfded51211
ossinova <oscar.dyremyhr@gmail.com>
2017-03-14 11:20:31 -0400

    Nav

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+
+		void OnGUI() {
+
+			if (GUI.Button (new Rect (400, 10, 400, 100), "Play")) {
+
+				SceneManager.LoadScene (1);
+
+			}
+
+			if (GUI.Button (new Rect (400, 200, 400, 100), "Instructions")) {
+
+				SceneManager.LoadScene (2);
+			}
+
+
+		}
+
+
+	}
